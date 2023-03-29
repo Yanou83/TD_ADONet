49a91b57c5e2d0b4bef54c42d448229ae81b78dd
Yann MONTENOT <montenot.yann@gmail.com>
2023-03-29 11:52:59 +0200

    Ajoutez des fichiers projet.

---
+++ TD_ADONet/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Oracle.ManagedDataAccess.Client;
+using MySql.Data.MySqlClient;
+
+
+namespace TD_ADONet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String host = "10.10.2.10";
+            int port = 1521;
+            string sid = "SLAM";
+            string login = "montenotado";
+            string pwd = "sio";
+            try
+            {
+                String cs = String.Format("Data Source= " + "(DESCRIPTION = (ADDRESS = (PROTOCOL = TCP)(HOST = {0})(PORT = {1}))" + "(CONNECT_DATA = (SERVICE_NAME = {2}))); User Id = {3}; Password = {4};", host, port, sid, login, pwd);
+
+                OracleConnection cnOracle = new OracleConnection(cs);
+                cnOracle.Open();
+                Console.WriteLine("Connecté Oracle");
+                cnOracle.Close();
+                Console.WriteLine("Déconnecté Oracle");
+            }
+            catch(OracleException ex)
+            {
+                Console.WriteLine("Erreur Oracle " + ex.Message);
+            }
+
+
+            string hostMysql = "127.0.0.1";
+            int portMysql = 3306;
+            string baseMysql = "dbadonet";
+            String uidMysql = "employeado";
+            String pwdMysql = "employeado123";
+            try
+            {
+                String csMysql = String.Format("Server = {0}; Port={1} ; Database = {2}; " + "Uid = {3}; " + "Pwd = {4}", hostMysql, portMysql, baseMysql, uidMysql, pwdMysql);
+                MySqlConnection cnMysql = new MySqlConnection(csMysql);
+                cnMysql.Open();
+                Console.WriteLine("connecté Mysql");
+                cnMysql.Close();
+                Console.WriteLine("déconnecté Mysql");
+            }
+            catch(MySqlException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ TD_ADONet/classes/EmployeOracle.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Oracle.ManagedDataAccess.Client;
+
+namespace EmployeDatas.Oracle
+{
+    class EmployeOracle
+    {
+        private string host;
+        private int port;
+        private string db;
+        private string login;
+        private string pwd;
+        private OracleConnection connexion;
+    }
+}
