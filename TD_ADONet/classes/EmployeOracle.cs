ffbb0599ce69d99d5b69853e898af6cb28f08b50
Yann MONTENOT <montenot.yann@gmail.com>
2023-03-30 16:01:47 +0200

    10%

--- TD_ADONet/classes/EmployeOracle.cs
+++ TD_ADONet/classes/EmployeOracle.cs
@@ -15,5 +15,44 @@ namespace EmployeDatas.Oracle
         private string login;
         private string pwd;
         private OracleConnection connexion;
+
+
+        public EmployeOracle(string host, int port, string db, string login, string pwd)
+        {
+            this.host = host;
+            this.port = port;
+            this.db = db;
+            this.login = login;
+            this.pwd = pwd;
+            connexion = new OracleConnection();
+        }
+
+        public void OuvrirOracle()
+        {
+            String cs = String.Format("Data Source= " + "(DESCRIPTION = (ADDRESS = (PROTOCOL = TCP)(HOST = {0})(PORT = {1}))" + "(CONNECT_DATA = (SERVICE_NAME = {2}))); User Id = {3}; Password = {4};", this.host, this.port, this.db, this.login, this.pwd);
+
+            connexion = new OracleConnection(cs);
+            connexion.Open();
+            Console.WriteLine("Connecté Oracle");
+ 
+        }
+
+        public void FermerOracle()
+        {
+            String cs = String.Format("Data Source= " + "(DESCRIPTION = (ADDRESS = (PROTOCOL = TCP)(HOST = {0})(PORT = {1}))" + "(CONNECT_DATA = (SERVICE_NAME = {2}))); User Id = {3}; Password = {4};", this.host, this.port, this.db, this.login, this.pwd);
+
+            connexion = new OracleConnection(cs);
+            connexion.Close();
+            Console.WriteLine("Déconnecté Oracle");
+        }
+
+        public void afficherTousLesCours()
+        {
+            this.OuvrirOracle();
+            string requete = "select * from cours";
+            this.connexion.CreateCommand(requete)
+
+        }
+
     }
 }
